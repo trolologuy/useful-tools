ac3e46d91bcc6e9c9c746abc291333a5d7cb4d18
trolologuy <16886290+trolologuy@users.noreply.github.com>
2020-05-25 23:36:03 +0200

    added link to the github repository + a few links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,16 @@
 module.exports = {
     title: 'Useful Tools',
     base: "/useful-tools/",
+    themeConfig: {
+      sidebar: 'auto',
+      nav: [
+        { text: 'Github', link: 'https://github.com/trolologuy/useful-tools' },
+      ]
+    },
     plugins: ['@vuepress/nprogress', '@vuepress/back-to-top', '@vuepress/active-header-links', {
         sidebarLinkSelector: '.sidebar-link',
         headerAnchorSelector: '.header-anchor'
       }, '@vuepress/search', {
         searchMaxSuggestions: 10
       }],
-    themeConfig: {
-        sidebar: 'auto'
-    }
 }
